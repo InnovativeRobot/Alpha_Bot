d40af06851ddaeafd0df07c95e8894600f423f95
SmartWebService <SmartWebService00@gmail.com>
2018-08-18 00:08:38 +0000

    first commit

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return "Welcome to Alpha BOT"
+    return render_template('default.html') 
+
+@app.route("/about", methods=["GET"])
+def about():
+    return  render_template('about.html') 
+
+
+
+@app.route('/showMap', methods=["GET"])
+def showMap():
+    return render_template('showMap.html') 
